b3b207118a39cd755bc4ce02d50638d538e6fb92
noah <noah@supergene.co>
2025-12-31 11:24:45 +0900

    Update.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout,
                              QFileDialog, QMessageBox, QScrollArea, QSplitter, QListWidget, QListWidgetItem, QTabWidget)
 
 
-VERSION = "20251226"
+VERSION = "20251231"
 
 def analyze_sharpness_chunk(args):
     """청크 단위로 선명도 분석 (별도 프로세스)"""
